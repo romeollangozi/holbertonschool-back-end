8a263e33d3e2a8a55a0fa6af50367a0e3f7f01ee
romeollangozi <romeollangozi123@gmail.com>
2023-12-18 17:32:36 +0100

    s

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Model to make a request to an
+API and retrieve data
+"""
+
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    URL = "https://jsonplaceholder.typicode.com/"
+    user_id = argv[1]
+    res = requests.get(f"{URL}users/{argv[1]}")
+    res = res.json()
+    user_name = res['name']
+
+    res = requests.get(f"{URL}todos")
+    all_todos = res.json()
+    user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
+    nr_tasks = len(user_todos)
+    completed_tasks = [completed for completed in user_todos
+                       if completed['completed'] is True]
+    completed_title = [title['title'] for title in completed_tasks]
+
+    print(f"Employee {user_name} is done", end="")
+    print(f" with tests({len(completed_tasks)}/{nr_tasks}):")
+    for title in completed_title:
+        print(f"\t {title}")
